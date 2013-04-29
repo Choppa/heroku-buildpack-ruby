0ec59e61ad3679614d7f5ae1d30f21f802275cb4
Sebastian Rachner <sebastian.rachner@blinkar.de>
2013-04-29 13:24:37 +0200

    Checking pkg-config for VIPS

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -434,16 +434,8 @@ ERROR
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         env_vars      += " BUNDLER_LIB_PATH=#{bundler_path}" if ruby_version == "ruby-1.8.7"
-        
-	ENV.each do |name, value|
-          puts "#{name}:#{value}"	
-	end
 
-	if File.exist?('vendor/vips/lib/libvips.a')
-	  puts "Found VIPS"
-	else
-	  puts "Couldn't find VIPS"
-	end
+	bundler_output << pipe("pkg-config --libs vips 2>&1")        
 
 	puts "Running: #{bundle_command}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
